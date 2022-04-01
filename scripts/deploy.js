c8197b6d2297479e709871d9f95ff679022b72b7
Yannis <yannis182@outlook.fr>
2022-04-01 13:48:50 +0200

    init hard hat project

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+const main = async () => {};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
