dd9a410b08237b34d467314c8657dabc152bf457
Yannis <yannis182@outlook.fr>
2022-04-01 20:05:27 +0200

    feature: progress on the CloneFactory contrac

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,24 @@
 const hre = require("hardhat");
 
-const main = async () => {};
+const main = async () => {
+  const bountyContractFactory = await hre.ethers.getContractFactory(
+    "contracts/Bounty.sol:Bounty"
+  );
+  const bountyContract = await bountyContractFactory.deploy();
+  await bountyContract.deployed();
+
+  console.log("bountyContract address: ", bountyContract.address);
+
+  const cloneContractFactory = await hre.ethers.getContractFactory(
+    "contracts/CloneFactory.sol:CloneFactory"
+  );
+  const cloneFactoryContract = await cloneContractFactory.deploy(
+    "0xdd2fd4581271e230360230f9337d5c0430bf44c0"
+  );
+
+  await cloneFactoryContract.deployed();
+  console.log("Clone Factory contract address: ", cloneFactoryContract.address);
+};
 
 const runMain = async () => {
   try {
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,25 @@
 const hre = require("hardhat");
 
-const main = async () => {};
+const main = async () => {
+  const bountyContractFactory = await hre.ethers.getContractFactory(
+    "contracts/Bounty.sol:Bounty"
+  );
+  const bountyContract = await bountyContractFactory.deploy();
+  await bountyContract.deployed();
+  // await bountyContract.initialize(bountyContract.address);
+
+  console.log("bountyContract address: ", bountyContract.address);
+
+  const cloneContractFactory = await hre.ethers.getContractFactory(
+    "contracts/CloneFactory.sol:CloneFactory"
+  );
+
+  const cloneFactoryContract = await cloneContractFactory.deploy(
+    bountyContract.address
+  );
+
+  console.log("the clone2", await cloneFactoryContract._clone());
+};
 
 const runMain = async () => {
   try {
